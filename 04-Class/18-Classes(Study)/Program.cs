f977641db4fe067d5b76d29aee95e8c20320c6ac
uluagac <ersinuluagac@gmail.com>
2024-10-01 09:19:21 +0300

    new project

---
+++ 04-Class/18-Classes(Study)/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace _18_Classes_Study_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
