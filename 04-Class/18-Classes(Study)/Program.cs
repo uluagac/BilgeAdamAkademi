e1da1f021ac2bb6a6ac8dad17aeb21a98329b8e6
Ersin Uluağaç <ersinuluagac@gmail.com>
2024-10-02 19:40:28 +0300

    edited

--- 04-Class/18-Classes(Study)/Program.cs
+++ 04-Class/18-Classes(Study)/Program.cs
@@ -4,6 +4,7 @@
     {
         static void Main(string[] args)
         {
+            GetStudent();
             Student student = new Student() { FirstName = "Recep", LastName = "Ersin", BirthDate = 1995, Id = 1905, Scholarship = true };
             Teacher teacher = new Teacher() { FirstName = "Murat", LastName = "Uluağaç", BirthDate = 1994, Id = 10044, Rank = "Prof", Salary = 105450 };
 
@@ -19,6 +20,21 @@
             foreach (Teacher tc in teachers)
             {
                 Console.WriteLine($"ID: {tc.Id} | Ad: {tc.FullName} | Yaş: {tc.BirthDate} | Rank: {tc.Rank} | Salary: {tc.Salary}");
+            }
+        }
+        static void GetStudent()
+        {
+            Console.Write("Lütfen öğrenci adını girin: ");
+            string Name = Console.ReadLine().Trim();
+            if (!string.IsNullOrEmpty(Name))
+            {
+                Console.WriteLine(Name);
+                
+            }
+            else
+            {
+                Console.WriteLine("Hatalı bir giriş yapıldı, lütfen harfler ile giriş yapın.");
+                GetStudent();
             }
         }
     }
