c9e0987cb08f73d1f8dedb95b7d0a1949ddae233
uluagac <ersinuluagac@gmail.com>
2024-10-02 21:33:27 +0300

    Created a new study project

---
+++ 04-Class/18-GameTry(Study)/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace _18_GameTry_Study_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
